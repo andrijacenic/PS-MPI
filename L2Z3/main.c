7214b48c2e7b1011d051bc85e3e64e23009b2b85
Andrija Cenic <andrija.cenic@elfak.rs>
2023-04-08 20:47:14 +0200

    zad 3 lab 2

--- L2Z3/main.c
+++ L2Z3/main.c
@@ -5,3 +5,50 @@ vrstu matrice A. Odštampati identifikatore procesa koji pripadaju comm1 i čija
 elemenata primljene vrste matrice A manja od zadate vrednosti v
 */
 
+#include <mpi.h>
+#include <stdio.h>
+#include <math.h>
+#define MCW MPI_COMM_WORLD
+#define n 4
+#define m 6
+int main(int argc, char* argv[]) {
+
+	int size, rank, rankComm, sizeComm;
+	int i, j;
+	int A[m][n];
+	int localA[n];
+	int sum = 0;
+	int v = 10;
+	int isDiv3;
+
+	MPI_Comm comm1;
+
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MCW, &size);
+	MPI_Comm_rank(MCW, &rank);
+	isDiv3 = rank % 3;
+	MPI_Comm_split(MCW, isDiv3 == 0, rank, &comm1);
+	MPI_Comm_size(comm1, &sizeComm);
+	MPI_Comm_rank(comm1, &rankComm);
+
+	if (isDiv3 == 0) {
+		if (rankComm == 0) {
+			for (i = 0; i < m; i++) {
+				for (j = 0; j < n; j++) {
+					A[i][j] = rand() % 10;
+				}
+			}
+		}
+		MPI_Scatter(&A[0][0], n, MPI_INT, &localA[0], n, MPI_INT, 0, comm1);
+		sum = 0;
+		for (i = 0; i < n; i++) sum += localA[i];
+		if (sum > v) {
+			printf("rank : %2d sum : %3d\n", rank, sum);
+		}
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
